33e3c87b07827c82a7da2b41eac71518d8445c0f
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-10-17 19:49:22 +0300

    add datacontext

---
+++ WebApplicationPark/WebApplicationPark/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplicationPark.Models;
+
+namespace WebApplicationPark
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Login> Logins { get; set; }
+    }
+}
--- WebApplicationPark/WebApplicationPark/Program.cs
+++ WebApplicationPark/WebApplicationPark/Program.cs
@@ -1,3 +1,11 @@
+using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using WebApplicationPark;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,6 +20,14 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
