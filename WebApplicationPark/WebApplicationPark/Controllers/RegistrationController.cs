8ffefb408741488ad01ccdff27310f7bd7bbf3c4
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-10-24 15:26:19 +0300

    controller Registration changed

--- WebApplicationPark/WebApplicationPark/Controllers/RegistrationController.cs
+++ WebApplicationPark/WebApplicationPark/Controllers/RegistrationController.cs
@@ -20,37 +20,45 @@ namespace WebApplicationPark.Controllers
         [HttpPost]
         public IActionResult Check1(Employees model)
         {
-            if (ModelState.IsValid)
+            try
             {
-                // Проверка, существует ли пользователь с таким же логином
-                var existingUser = _context.Employees.FirstOrDefault(u => u.Login == model.Login);
-
-                if (existingUser != null)
+                if (ModelState.IsValid)
                 {
-                    ModelState.AddModelError("login", "Пользователь с таким логином уже существует.");
-                    return View(model);
+                    // Проверка, существует ли пользователь с таким же логином
+                    var existingUser = _context.Employees.FirstOrDefault(u => u.Login == model.Login);
+
+                    if (existingUser != null)
+                    {
+                        ModelState.AddModelError("login", "Пользователь с таким логином уже существует.");
+                        return View(model);
+                    }
+
+                    // Создание нового пользователя
+                    var newUser = new Employees
+                    {
+                        Login = model.Login,
+                        Password = model.Password,
+                        FirstName = model.FirstName,
+                        LastName = model.LastName,
+                        DateOfBirth = model.DateOfBirth,
+                        PositionID = model.PositionID
+                    };
+
+                    // Добавление пользователя в базу данных
+                    _context.Employees.Add(newUser);
+                    _context.SaveChanges();
+
+                    // Перенаправление на главную страницу или на страницу входа
+                    return RedirectToAction("Index", "Home"); // Пример перенаправления на главную страницу
                 }
 
-                // Создание нового пользователя
-                var newUser = new Employees
-                {
-                    Login = model.Login,
-                    Password = model.Password,
-                    FirstName = model.FirstName,
-                    LastName = model.LastName,
-                    DateOfBirth = model.DateOfBirth
-                };
-
-                // Добавление пользователя в базу данных
-                _context.Employees.Add(newUser);
-                _context.SaveChanges();
-
-                // Перенаправление на главную страницу или на страницу входа
-                return RedirectToAction("Index", "Home"); // Пример перенаправления на главную страницу
+                // Если модель недействительна, верните пользователя на страницу регистрации с ошибками
+                return View(model);
             }
-
-            // Если модель недействительна, верните пользователя на страницу регистрации с ошибками
-            return View(model);
+            catch (InvalidOperationException ex)
+            {
+                return View("Error", new ErrorViewModel { Message = ex.Message });
+            }
         }
     }
 }
