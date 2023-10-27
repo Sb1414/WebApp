f7525227b23e7cca68b9cfd23847c87810b47ac4
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-10-27 20:28:30 +0300

    .

--- WebApplicationPark/WebApplicationPark/Models/Employees.cs
+++ WebApplicationPark/WebApplicationPark/Models/Employees.cs
@@ -8,30 +8,32 @@ public class Employees
     [Key]
     public int Id { get; set; }
 
-    [Required(ErrorMessage ="введите логин")]
-    [Display(Name = "введите логин")]
-    [StringLength(100, MinimumLength = 3)]
-    [Remote(action: "VerifyLogin", controller: "Employees")]
+    [Required(ErrorMessage = "Введите логин")]
+    [Display(Name = "Логин")]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "Логин должен содержать не менее 3 символов.")]
+    [Remote(action: "VerifyLogin", controller: "Registration", ErrorMessage = "Пользователь с таким логином уже существует.")]
     public string Login { get; set; }
 
-    [Required(ErrorMessage = "введите пароль")]
-    [Display(Name = "введите пароль")]
-    [StringLength(50, MinimumLength = 2)]
+    [Required(ErrorMessage = "Введите пароль")]
+    [Display(Name = "Пароль")]
+    [StringLength(100, MinimumLength = 8, ErrorMessage = "Пароль должен содержать не менее 8 символов.")]
+    [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]+$", ErrorMessage = "Пароль должен содержать буквы и цифры без пробелов.")]
     public string Password { get; set; }
 
-    [Required(ErrorMessage = "введите имя")]
-    [Display(Name = "введите имя")]
-    [StringLength(100)]
+    [Required(ErrorMessage = "Введите имя")]
+    [Display(Name = "Имя")]
+    [RegularExpression(@"^[A-ZА-Я][a-zа-я]*$", ErrorMessage = "Имя должно начинаться с заглавной буквы и содержать только буквы.")]
     public string FirstName { get; set; }
 
-    [Required(ErrorMessage = "введите фамилию")]
-    [Display(Name = "введите фамилию")]
-    [StringLength(100)]
+    [Required(ErrorMessage = "Введите фамилию")]
+    [Display(Name = "Фамилия")]
+    [RegularExpression(@"^[A-ZА-Я][a-zа-я]*$", ErrorMessage = "Фамилия должна начинаться с заглавной буквы и содержать только буквы.")]
     public string LastName { get; set; }
 
-    [Required(ErrorMessage = "введите дату рождения")]
-    [Display(Name = "введите дату рождения")]
-    public string DateOfBirth { get; set; }
+    [Required(ErrorMessage = "Введите дату рождения")]
+    [Display(Name = "Дата рождения")]
+    [DataType(DataType.Date)]
+    public DateTime DateOfBirth { get; set; }
 
     public int PositionID { get; set; }
 
