53d81a9d5275ca35d872c0ab5dd26ec0f3c8a8c1
Evette Isadora <evetteis@si-e4.kzn.21-school.ru>
2023-10-27 20:03:45 +0300

    RegistrationController.cs

--- WebApplicationPark/WebApplicationPark/Controllers/RegistrationController.cs
+++ WebApplicationPark/WebApplicationPark/Controllers/RegistrationController.cs
@@ -25,49 +25,45 @@ namespace WebApplicationPark.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Check1(Employees model)
         {
             try
             {
-                // if (ModelState.IsValid)
-                //{
-                    // Проверка, существует ли пользователь с таким же логином
-                    var existingUser = _context.Employees.FirstOrDefault(u => u.Login == model.Login);
+                if(!ModelState.IsValid)
+                {
+                    // ViewData["FirstNameError"] = "Ошибка ввода имени";
+                    return View("Index");
+                    // return View(model); 
+                }
+                
+                // Проверка, существует ли пользователь с таким же логином
+                var existingUser = _context.Employees.FirstOrDefault(u => u.Login == model.Login);
 
-                    if (existingUser != null)
-                    {
-                        ViewBag.ErrorMessage = "Пользователь с таким логином уже существует.";
+                if (existingUser != null)
+                {
+                    ViewBag.ErrorMessage = "Пользователь с таким логином уже существует.";
 
-                        return View();
-                    }
+                    return View();
+                }
 
-                    // Создание нового пользователя
-                    var newUser = new Employees
-                    {
-                        Login = model.Login,
-                        Password = model.Password,
-                        FirstName = model.FirstName,
-                        LastName = model.LastName,
-                        DateOfBirth = model.DateOfBirth,
-                        PositionID = model.PositionID
-                    };
-                /*
-                    if (newUser != null)
-                    {
-                        Console.WriteLine("\n === 4 ========================> newUser не ноль \n");
-                    }
-                    Console.WriteLine("\n === 4 ========================> создали \n");
-                */
-                    // Добавление пользователя в базу данных
-                    _context.Employees.Add(newUser);
-                    _context.SaveChanges();
+                // Создание нового пользователя
+                var newUser = new Employees
+                {
+                    Login = model.Login,
+                    Password = model.Password,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    DateOfBirth = model.DateOfBirth,
+                    PositionID = model.PositionID
+                };
+                
+                // Добавление пользователя в базу данных
+                _context.Employees.Add(newUser);
+                _context.SaveChanges();
 
-                    // Перенаправление на главную страницу или на страницу входа
-                    return RedirectToAction("Index", "Home"); // Пример перенаправления на главную страницу
-                //}
-
-                // Если модель недействительна, верните пользователя на страницу регистрации с ошибками
-                // return View(model);
+                // Перенаправление на главную страницу или на страницу входа
+                return RedirectToAction("Index", "Home"); // Пример перенаправления на главную страницу
             }
             catch (Exception ex)
             {
