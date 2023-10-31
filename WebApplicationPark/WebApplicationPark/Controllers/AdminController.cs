feb148c7fa5f482459b76285f4adbb1f7acc7b46
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-10-31 18:08:48 +0300

    admin controller

--- WebApplicationPark/WebApplicationPark/Controllers/AdminController.cs
+++ WebApplicationPark/WebApplicationPark/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using WebApplicationPark.Models;
 
 namespace WebApplicationPark.Controllers;
@@ -14,6 +15,7 @@ public class AdminController : Controller
     
     public IActionResult EditEmployee(int id)
     {
+        ViewBag.Positions = new SelectList(_context.Positions, "Position", "Position");
         var employee = _context.Employees.FirstOrDefault(e => e.Id == id);
 
         if (employee == null)
@@ -35,6 +37,11 @@ public class AdminController : Controller
         //}
         Console.WriteLine("\n\n\n\nya tyt\n\n\n\n");
         var existingEmployee = _context.Employees.FirstOrDefault(e => e.Id == model.Id);
+        
+        var positionName = model.Position.Position;
+        var position = _context.Positions.FirstOrDefault(p => p.Position == positionName);
+        var positionId = position.id;
+        
 
         if (existingEmployee == null)
         {
@@ -45,9 +52,28 @@ public class AdminController : Controller
         existingEmployee.FirstName = model.FirstName;
         existingEmployee.LastName = model.LastName;
         existingEmployee.DateOfBirth = model.DateOfBirth;
-        existingEmployee.PositionID = model.PositionID;
+        existingEmployee.PositionID = positionId;
 
         _context.SaveChanges(); // Сохраните изменения в базе данных
+
+        return RedirectToAction("Index");
+    }
+    
+    [HttpPost]
+    public IActionResult DeleteEmployee(int id)
+    {
+        var employeeToDelete = _context.Employees.FirstOrDefault(e => e.Id == id);
+
+        Console.WriteLine("\n\n\n\nDeleteEmployee\n\n\n\n");
+        
+        if (employeeToDelete == null)
+        {
+            Console.WriteLine("\n\n\n\nОбработка случая, если сотрудник не найден\n\n\n\n");
+            return NotFound(); // Обработка случая, если сотрудник не найден
+        }
+
+        _context.Employees.Remove(employeeToDelete);
+        _context.SaveChanges(); // Удаление сотрудника из базы данных
 
         return RedirectToAction("Index");
     }
@@ -59,7 +85,6 @@ public class AdminController : Controller
        //     .Join(_context.Positions, e => e.PositionID, p => p.id, (e, p) => new { Employee = e, Position = p })
         //    .Select(x => new { FirstName = x.Employee.FirstName, LastName = x.Employee.LastName, DateOfBirth = x.Employee.DateOfBirth, Position = x.Position.Position })
          //   .ToList();
-        
          var employees = _context.Employees
             .Join(_context.Positions, e => e.PositionID, p => p.id, (e, p) => new { Id = e.Id, FirstName = e.FirstName, LastName = e.LastName, DateOfBirth = e.DateOfBirth, Position = p.Position })
             .ToList();
