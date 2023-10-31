8eba6acceb54fcd5943a868ec380201c20644f67
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-10-31 18:09:03 +0300

    models

--- WebApplicationPark/WebApplicationPark/Models/Positions.cs
+++ WebApplicationPark/WebApplicationPark/Models/Positions.cs
@@ -7,11 +7,9 @@ public class Positions
     [Key]
     public int id { get; set; }
     
-    [Required(ErrorMessage = "Введите позицию")]
     [Display(Name = "Позиция")]
     public string Position { get; set; }
     
-    [Required(ErrorMessage = "Введите зарплату")]
     [Display(Name = "Зарплата")]
     public string Salary { get; set; }
 }
